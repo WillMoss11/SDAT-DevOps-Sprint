a7f30b322357d4be77a14379dba779a1fa02aa2c
WillMoss11 <william.moss@keyin.com>
2025-02-26 18:10:16 -0330

    DTO Added

--- demo/src/main/java/com/example/AirportController.java
+++ demo/src/main/java/com/example/AirportController.java
@@ -14,8 +14,15 @@ public class AirportController {
     private CityRepository cityRepository;
 
     @GetMapping
-    public List<Airport> getAllAirports() {
-        return airportRepository.findAll();
+    public List<AirportDTO> getAllAirports() {
+        List<Airport> airports = airportRepository.findAll();
+        List<AirportDTO> airportDTOs = new ArrayList<>();
+
+        for (Airport airport : airports) {
+            airportDTOs.add(new AirportDTO(airport.getId(), airport.getName(), airport.getCode(), airport.getCity().getName()));
+        }
+
+        return airportDTOs;
     }
 
     @GetMapping("/{id}")
